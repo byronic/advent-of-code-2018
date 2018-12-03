23cd503aad1242fbe60b63c606ac7a995860b369
byronic <byron.lagrone@cotiviti.com>
2018-12-02 22:35:13 -0600

    1.1 Solution, 1.2 Failure
    1.1 has a working solution, 1.2 fails. Adds a readme

---
+++ 1/Frequency.scala
@@ -0,0 +1,28 @@
+import scala.io.Source
+
+object Frequency {
+val bufferedSource = Source.fromFile("dataset")
+val lines = bufferedSource.getLines.toList
+bufferedSource.close
+
+def frequency(l: List[String]): Int = {
+  @annotation.tailrec
+  def go(l: List[String], curr: Int): Int = {
+    if (l.isEmpty) curr
+    else go(l.tail, curr + l.head.toInt)
+  }
+  go(l, 0)
+}
+
+def firstTwice(l: List[String]): Int = {
+  def go(l: List[String], fs: List[Int], curr: Int): Int = {
+    if (fs.contains(curr)) curr
+    else if (l.tail.isEmpty) curr
+    else go(l.tail, (curr) :: fs, curr + l.head.toInt)
+  }
+  go(l, List(), 0)
+}
+
+frequency(lines)
+
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+# Advent of Code 2018
+
+[Advent of Code](https://adventofcode.com) puzzles for 2018.
+
+Warning: I am bad at these. Expect horrible, stupid things that _might_ also
+be solutions.
+
